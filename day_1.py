1486a3d70b7409fda60bfd9d54c39436062f9f59
AlvinThalupadath <alvinthalupadath@gmail.com>
2025-08-19 14:28:05 -0700

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,7 @@
+import random
+
+def favorite_player():
+    players = ("Lando Norris", "Oscor Piastri", "Max Verstappen", "Yuki","George Russell")
+    random = random.randint(0,-1)
+    print(favorite_player)
+    
